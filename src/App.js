52a0c28c28e826713c0e0c667435595749676818
Farahksyu <ksyu.farah@gmail.com>
2024-06-27 13:53:14 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import './App.css';
+import "./App.css";
+import { getMovieList , searchMovie } from "./api";
+import { useEffect, useState} from "react";
 
-function App() {
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img alt="" className="Movie-image" 
+          src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}></img>
+          <div className="Movie-date">{movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
+  const search = (q) => {
+    console.log({q})
+  }
+
+console.log({popularMovies: popularMovies})
+
   return (
     <div className="App">
       <header className="App-header">
-         <h1>KSYU MOVIE POINT</h1>
-         <div className="Movie-container">
-          <div className="Movie-wrapper">
-            <div className="Movie-title"></div>
-            <img className="Movie-image"></img>
-            <div className="Movie-date"></div>
-            <div className="Movie-rate"></div>
-          </div>
-         </div>
+        <h1>KSYU MOVIE POINT</h1>
+        <input 
+          placeholder='cari film kesukaanmu' 
+          className="Movie-search"
+          onChange={({target}) => search(target.value)}>
+        </input>
+        <div className="Movie-container">
+          <PopularMovieList></PopularMovieList>
+        </div>
       </header>
     </div>
   );
